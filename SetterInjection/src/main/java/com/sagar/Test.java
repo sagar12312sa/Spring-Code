38ffa157a6f1319f4d35fdc0746caf241d997bc9
Sagar12312sa <sagarhp2560@gmail.com>
2024-01-18 20:35:19 +0530

    Setter Injectiom

---
+++ SetterInjection/src/main/java/com/sagar/Test.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sagar;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author sagar
+ */
+public class Test {
+    public static void main(String[] args){
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Employee employee =(Employee) context.getBean("employee");
+        employee.display();
+        Employee employee1 =(Employee) context.getBean("employee1");
+        employee1.display();
+    }
+}
